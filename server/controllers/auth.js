870c1517932d6efaf0b1813dbe94772e63ff7c27
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-02-20 13:48:11 -0500

    added some hash

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -7,6 +7,10 @@ exports.signup = function(req, res, next){
 	var email = req.body.email;
 	var password = req.body.password;
 
+		if( !email || ! password){
+			return res.status(418).send({error: 'You must provide email and password'});
+		}
+
 	//2
 	User.findOne({ email: email }, function(err, existingUser){
 		if(err) {
