97fd535ca63380a28e404113d5459699758945c2
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-02-20 14:14:41 -0500

    updated authentication

--- server/router.js
+++ server/router.js
@@ -1,11 +1,16 @@
 module.exports = function(app){
 
 	var Auth = require('./controllers/auth');
-	var User = require('./models/user');
+	var passportService = require('./services/passport');
+	var passport = require('passport');
 
+	// var User = require('./models/user');
 
-	app.get('/', function(req, res, next){
+var requireAuth = passport.authenticate('jwt', { session: false });
+
+	app.get('/', requireAuth, function(req, res){
 			res.send("HELLOOOOOO HOMEPAGE");
+			//res.send({hi: 'there'});
 	});
 
 	app.post('/signup', Auth.signup);
