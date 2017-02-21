28920c0ef75eac9c6933699b4bfb441ad0fcaf15
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-02-21 10:51:32 -0500

    added props

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import JSXIntro from './1_JSXIntro';
 import ComponentsIntro from './2_ComponentsIntro';
+import PropsIntro from './3_PropsIntro';
 
 	export default class App extends Component {
 		render(){
@@ -9,6 +10,7 @@ import ComponentsIntro from './2_ComponentsIntro';
 					<h1>React Intro</h1>
 					<JSXIntro />
 					<ComponentsIntro />
+					<PropsIntro name="Michael Bolton" title="The Neverending Elevator Music" />
 				</div>
 				);
 		}
